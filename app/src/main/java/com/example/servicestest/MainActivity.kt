092a4ac1f9122eb54e6c5b02456b770ef079f4e8
Ivan Usatov <andrappdevelop@gmail.com>
2023-01-21 13:15:26 +0700

    add service counter

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.servicestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this, 25))
+        }
     }
 }
